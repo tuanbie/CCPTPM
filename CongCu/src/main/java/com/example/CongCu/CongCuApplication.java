698b0e1c3f347cc4656f04f1dc70990da540eafa
VanTuan <84395117+tuanbie@users.noreply.github.com>
2023-03-09 10:09:37 +0700

    commit lan dau

--- CongCu/src/main/java/com/example/CongCu/CongCuApplication.java
+++ CongCu/src/main/java/com/example/CongCu/CongCuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CongCuApplication {
-//su LAN DAU
+//lan dau
 	public static void main(String[] args) {
 		SpringApplication.run(CongCuApplication.class, args);
 	}
