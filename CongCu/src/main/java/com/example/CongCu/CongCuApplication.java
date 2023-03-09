9f732a879ec8b37acbb62a1f30236b786cad9507
VanTuan <84395117+tuanbie@users.noreply.github.com>
2023-03-09 10:17:47 +0700

    commit dev

--- CongCu/src/main/java/com/example/CongCu/CongCuApplication.java
+++ CongCu/src/main/java/com/example/CongCu/CongCuApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CongCuApplication {
 //lan dau
+	//sua dev
 	public static void main(String[] args) {
 		SpringApplication.run(CongCuApplication.class, args);
 	}
