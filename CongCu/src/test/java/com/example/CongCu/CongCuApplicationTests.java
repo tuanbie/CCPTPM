6b145ae74a6174bddf4595fd236ea27abbcea20b
VanTuan <84395117+tuanbie@users.noreply.github.com>
2023-03-09 09:43:46 +0700

    NguyenVanTuan_1911060307

---
+++ CongCu/src/test/java/com/example/CongCu/CongCuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CongCu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongCuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
